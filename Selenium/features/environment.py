cab8c36cf14b06789b2bba1600e8ffb300393d66
Dmytro Kachan <83279757+DimitriyKachan@users.noreply.github.com>
2021-12-24 18:27:03 +0200

    Update environment.py

--- Selenium/features/environment.py
+++ Selenium/features/environment.py
@@ -4,3 +4,7 @@ from main import Driver
 @fixture
 def before_all(context):
     context.drive = Driver()
+    context.name = "Smith"
+    context.currency = "CAD - Canadian Dollar"
+    context.min = "1500"
+    context.max = "3000"
