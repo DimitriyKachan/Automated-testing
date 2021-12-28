a06a27dd557f0d23ce26789d88f9a6dd32604b0e
Dmytro Kachan <83279757+DimitriyKachan@users.noreply.github.com>
2021-12-28 09:35:22 +0200

    Update environment.py

--- WebApi/features/environment.py
+++ WebApi/features/environment.py
@@ -6,3 +6,4 @@ def before_all(context):
     context.dbx = DRBOX("jDoTGWe41tkAAAAAAAAAATWqAbof201De4JPxNDczuVnl0-huGifwlqUjiFaw3h7")
     context.db_path = '/test_dropbox/my-file.txt'
     context.local_path = 'my-file.txt'
+    context.hash = "6680bbec0d05d3eaac9c8b658c40f28d2f0cb0f245c7b1cabf5a61c35bd03d8e"
